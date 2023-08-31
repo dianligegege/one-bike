d4dd89fab11a96c45e9ac2093fef63e3b103e591
zhangli <zhangli02@hongsong.club>
2023-08-31 19:13:50 +0800

    fix: 恢复v console

--- src/components/GetQrcode.vue
+++ src/components/GetQrcode.vue
@@ -6,27 +6,22 @@
     </div>
     <button @click="openCamera">开启摄像头</button>
     <div v-if="state.isUseTorch" class="track" @click="openTrack">
-      {{ state.trackStatus ? "关闭闪光灯" : "打开闪光灯" }}
+      {{ state.trackStatus ? '关闭闪光灯' : '打开闪光灯' }}
     </div>
   </div>
 </template>
 
 <script setup>
-import jsQR from "jsqr";
-import {
-  nextTick,
-  reactive,
-  onMounted,
-  ref,
-} from 'vue';
+import jsQR from 'jsqr'
+import { nextTick, reactive, onMounted, ref } from 'vue'
 
 const linkMap = {
-  'didi': 'blackhorse://qrscan',
-  'hello': 'hellobike://hellobike.com/scan_qr',
-  'mobike': 'imeituan://www.meituan.com/scanQRCode'
-};
+  didi: 'blackhorse://qrscan',
+  hello: 'hellobike://hellobike.com/scan_qr',
+  mobike: 'imeituan://www.meituan.com/scanQRCode'
+}
 
-const videoRef = ref(null);
+const videoRef = ref(null)
 
 const state = reactive({
   video: null,
@@ -37,47 +32,36 @@ const state = reactive({
   cline2d: null,
   trackStatus: false,
   track: null,
-  isUseTorch: true,
-});
+  isUseTorch: true
+})
 
 const handleVideo = () => {
   if (videoRef.value.readyState !== videoRef.value.HAVE_ENOUGH_DATA) {
-    return requestAnimationFrame(handleVideo);
-  };
-  if (!state.canvas2d) {
-    state.canvas2d = state.c.getContext("2d");
+    return requestAnimationFrame(handleVideo)
   }
-  state.canvas2d.drawImage(
-    videoRef.value,
-    0,
-    0,
-    state.canvasWidth,
-    state.canvasHeight
-  );
+  if (!state.canvas2d) {
+    state.canvas2d = state.c.getContext('2d')
+  }
+  state.canvas2d.drawImage(videoRef.value, 0, 0, state.canvasWidth, state.canvasHeight)
 
-  const imageData = state.canvas2d.getImageData(
-    0,
-    0,
-    state.canvasWidth,
-    state.canvasHeight
-  );
+  const imageData = state.canvas2d.getImageData(0, 0, state.canvasWidth, state.canvasHeight)
 
   const code = jsQR(imageData.data, imageData.width, imageData.height, {
-    inversionAttempts: "dontInvert",
-  });
+    inversionAttempts: 'dontInvert'
+  })
 
   if (code && code.data) {
-    getData(code.data);
-    return;
+    getData(code.data)
+    return
   }
 
-  requestAnimationFrame(handleVideo);
-};
+  requestAnimationFrame(handleVideo)
+}
 
 const getData = (data) => {
   Object.entries(linkMap).forEach(([key, value]) => {
     if (data.includes(key)) {
-      window.location = value;
+      window.location = value
     }
   })
 }
@@ -88,45 +72,41 @@ const openCamera = () => {
     video: {
       facingMode: { exact: 'environment' },
       width: state.canvasHeight,
-      height: state.canvasWidth * 1.5,
-    },
-  };
-  navigator.mediaDevices
-    .getUserMedia(videoParam)
-    .then((stream) => {
-      videoRef.value.srcObject = stream;
-      videoRef.value.setAttribute("playsinline", true);
-      videoRef.value.play();
-      window.requestAnimationFrame(handleVideo);
+      height: state.canvasWidth * 1.5
+    }
+  }
+  navigator.mediaDevices.getUserMedia(videoParam).then((stream) => {
+    videoRef.value.srcObject = stream
+    videoRef.value.setAttribute('playsinline', true)
+    videoRef.value.play()
+    window.requestAnimationFrame(handleVideo)
+    ;[state.track] = stream.getVideoTracks()
 
-      [state.track] = stream.getVideoTracks();
-
-      setTimeout(() => {
-        state.isUseTorch = state.track.getCapabilities().torch || null;
-      }, 500);
-    })
-};
+    setTimeout(() => {
+      state.isUseTorch = state.track.getCapabilities().torch || null
+    }, 500)
+  })
+}
 
 const openTrack = () => {
-  state.trackStatus = !state.trackStatus;
+  state.trackStatus = !state.trackStatus
   state.track.applyConstraints({
-    advanced: [{ torch: state.trackStatus }],
-  });
-};
+    advanced: [{ torch: state.trackStatus }]
+  })
+}
 
 onMounted(() => {
-  state.canvasWidth = window.screen.availWidth;
-  state.canvasHeight = window.screen.availHeight;
+  state.canvasWidth = window.screen.availWidth
+  state.canvasHeight = window.screen.availHeight
 
   nextTick(() => {
-    state.c = document.createElement("canvas");
-    state.c.id = "c";
-    state.c.width = state.canvasWidth;
-    state.c.height = state.canvasHeight;
-    state.c.style.width = "100%";
-  });
-});
-
+    state.c = document.createElement('canvas')
+    state.c.id = 'c'
+    state.c.width = state.canvasWidth
+    state.c.height = state.canvasHeight
+    state.c.style.width = '100%'
+  })
+})
 </script>
 
 <style lang="scss" scoped>
@@ -144,7 +124,6 @@ onMounted(() => {
   }
 
   .line-wrap {
-    display: none;
     position: absolute;
     top: 0;
     left: 0;
@@ -156,7 +135,6 @@ onMounted(() => {
     pointer-events: none;
     z-index: 1;
     overflow: hidden;
-    background-color: #ccc;
 
     .line {
       height: calc(100% - 2px);
@@ -164,7 +142,7 @@ onMounted(() => {
       background: linear-gradient(179deg, rgba(0, 255, 51, 0) 43%, #a4ebb2a6 211%);
       border-bottom: 1px solid #a4ebb2a6;
       transform: translateY(-100%);
-      // animation: radar-beam 4s infinite normal;
+      animation: radar-beam 4s infinite normal;
       animation-timing-function: linear;
       animation-delay: 1s;
     }
